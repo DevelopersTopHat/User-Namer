cb7f1ef5157de6a97767a2163d35d8199b7e4e4b
Abdeali Daginawala <abdeali1997@gmail.com>
2017-06-27 18:25:24 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,18 @@ var radios = document.querySelectorAll("input[type='radio']");
 var number;
 var theName ="";
 var theLength = 8;
+var submit = document.querySelector("input[type='submit']");
+
+submit.addEventListener("click",getBaseString);
+
+username.addEventListener("keypress",function(e){
+	if(e.which === 13 ){
+		getBaseString();	
+	}
+});
 
 function getBaseString() {
+	console.log("get base string");
 	number = Math.floor(Math.random() * 10);
 	if(username.value){
 		theName = username.value;
@@ -16,13 +26,14 @@ function getBaseString() {
 		else{
 			if(containANumber.checked){
 				if(suffix.checked){
-						theName += number;
+					theName += number;
 				}
 				if(prefix.checked){
 					theName = number+theName;
 				}
 			}
 			p.textContent = "your name is "+theName;
+			console.log("your name is "+theName);
 		}
 	}
 }
