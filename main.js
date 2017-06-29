f4b65443aca7997eb0e44c75215d38ebf7c7eb77
Abdeali Daginawala <abdeali1997@gmail.com>
2017-06-29 02:17:07 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 var username = document.getElementById("username");
 var p = document.querySelector("p");
+
 var containANumber = document.getElementById("containANumber");
-var radios = document.querySelectorAll("input[type='radio']");
+var containASpecial = document.getElementById("containASpecial");
+
+var radioNum = [document.getElementById("prefix"), document.getElementById("suffix")];
+var radioSpecial = [document.getElementById("sprefix"), document.getElementById("ssuffix")];
+
 var number;
+var specialChar;
+
 var theName ="";
 var theLength = 8;
 var submit = document.querySelector("input[type='submit']");
 
 submit.addEventListener("click",getBaseString);
+containANumber.addEventListener("click", setRadioBtn);
+containASpecial.addEventListener("click", setRadioSpecial)
 
 username.addEventListener("keypress",function(e){
 	if(e.which === 13 ){
@@ -15,9 +24,12 @@ username.addEventListener("keypress",function(e){
 	}
 });
 
+
+
 function getBaseString() {
 	console.log("get base string");
 	number = Math.floor(Math.random() * 10);
+	specialChar = getSpecialChar();
 	if(username.value){
 		theName = username.value;
 		if(theName.length > theLength){
@@ -32,17 +44,54 @@ function getBaseString() {
 					theName = number+theName;
 				}
 			}
+			if(containASpecial.checked) {
+				if (ssuffix.checked) {
+					theName += specialChar;
+				} else if (sprefix.checked) {
+					theName = specialChar + theName;
+				}
+			} 
 			p.textContent = "your name is "+theName;
 			console.log("your name is "+theName);
 		}
 	}
 }
 
+//radio buttons for numbers
 function setRadioBtn() {
-		for(var i=0; i<radios.length; i++){
-			radios[i].disabled = !radios[i].disabled;
+	if(containANumber.checked) {
+		for(var i=0; i<radioNum.length; i++){
+			radioNum[i].disabled = radioNum[i].enabled;	
 		}
+	}
+	else {
+		for(var i=0; i<radioNum.length; i++){
+			radioNum[i].enabled = radioNum[i].disabled;	
+		}
+	}
+
 }
+
+//radio buttons for special characters
+function setRadioSpecial() {
+	if(containANumber.checked) {
+		for(var i=0; i<radioSpecial.length; i++){
+			radioSpecial[i].disabled = radioSpecial[i].enabled;	
+		}
+	}
+	else {
+		for(var i=0; i<radioSpecial.length; i++){
+			radioSpecial[i].enabled = radioSpecial[i].disabled;	
+		}
+	}
+}
+
 function hide(strlength){
 	theLength = strlength;
+}
+
+function getSpecialChar() {
+	var specialCharList = ["!",	"#", "$", "%", "&", "'", "*", "+", "-",	".", ":", "<", "=",	">", "?", "@", "[", "]", "^", "_", "`",	"{", "|", "}", "~"];
+	var sNumber = Math.floor(Math.random() * 10);
+	return specialCharList[sNumber];
 }
